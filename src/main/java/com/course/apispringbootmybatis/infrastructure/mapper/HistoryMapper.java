4e9046524b241e11189aa78483e2d9273876cccc
toranoko114 <toranoko114.business@gmail.com>
2023-01-27 22:27:44 +0900

    利用していないメソッドの削除

--- src/main/java/com/course/apispringbootmybatis/infrastructure/mapper/HistoryMapper.java
+++ src/main/java/com/course/apispringbootmybatis/infrastructure/mapper/HistoryMapper.java
@@ -1,26 +1,15 @@
 package com.course.apispringbootmybatis.infrastructure.mapper;
 
-import com.course.apispringbootmybatis.domain.dto.HistoryDto;
 import com.course.apispringbootmybatis.domain.entity.HistoryEntity;
 import java.util.List;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 /**
  * Historyテーブル Mapperクラス
  */
 @Mapper
 public interface HistoryMapper {
-
-  /**
-   * 社員IDに該当する社員履歴情報リストの取得.
-   *
-   * @param employeeId 社員ID
-   * @return 社員履歴情報リスト
-   */
-  @Select("SELECT * FROM HISTORY WHERE EMPLOYEE_ID = #{employeeId}")
-  List<HistoryDto> selectById(String employeeId);
 
   /**
    * 社員履歴情報リストの一括登録・更新.
